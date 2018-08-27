69e485f579269035003c04b63241614d08f06ce4
Siphamandla brian Mjoli <g14m1190@campus.ru.ac.za>
2018-08-27 15:39:57 +0200

    intial commit

---
+++ js/main.js
@@ -0,0 +1,542 @@
+/* ===================================================================
+ * Transcend - Main JS
+ *
+ * ------------------------------------------------------------------- */
+
+(function($) {
+
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    },
+
+    $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+    var clPreloader = function() {
+        
+        $("html").addClass('cl-preload');
+
+        $WIN.on('load', function() {
+
+            //force page scroll position to top at page refresh
+            // $('html, body').animate({ scrollTop: 0 }, 'normal');
+
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function() {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+            
+            // for hero content animations 
+            $("html").removeClass('cl-preload');
+            $("html").addClass('cl-loaded');
+        
+        });
+    };
+
+
+   /* Menu on Scrolldown
+    * ------------------------------------------------------ */
+    var clMenuOnScrolldown = function() {
+        
+        var menuTrigger = $('.header-menu-toggle');
+
+        $WIN.on('scroll', function() {
+
+            if ($WIN.scrollTop() > 150) {
+                menuTrigger.addClass('opaque');
+            }
+            else {
+                menuTrigger.removeClass('opaque');
+            }
+
+        });
+    };
+
+
+   /* OffCanvas Menu
+    * ------------------------------------------------------ */
+    var clOffCanvas = function() {
+
+        var menuTrigger     = $('.header-menu-toggle'),
+            nav             = $('.header-nav'),
+            closeButton     = nav.find('.header-nav__close'),
+            siteBody        = $('body'),
+            mainContents    = $('section, footer');
+
+        // open-close menu by clicking on the menu icon
+        menuTrigger.on('click', function(e){
+            e.preventDefault();
+            siteBody.toggleClass('menu-is-open');
+        });
+
+        // close menu by clicking the close button
+        closeButton.on('click', function(e){
+            e.preventDefault();
+            menuTrigger.trigger('click');
+        });
+
+        // close menu clicking outside the menu itself
+        siteBody.on('click', function(e){
+            if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
+                siteBody.removeClass('menu-is-open');
+            }
+        });
+
+    };
+
+
+   /* photoswipe
+    * ----------------------------------------------------- */
+    var clPhotoswipe = function() {
+        var items = [],
+            $pswp = $('.pswp')[0],
+            $folioItems = $('.item-folio');
+
+        // get items
+        $folioItems.each( function(i) {
+
+            var $folio = $(this),
+                $thumbLink =  $folio.find('.thumb-link'),
+                $title = $folio.find('.item-folio__title'),
+                $caption = $folio.find('.item-folio__caption'),
+                $titleText = '<h4>' + $.trim($title.html()) + '</h4>',
+                $captionText = $.trim($caption.html()),
+                $href = $thumbLink.attr('href'),
+                $size = $thumbLink.data('size').split('x'),
+                $width  = $size[0],
+                $height = $size[1];
+        
+            var item = {
+                src  : $href,
+                w    : $width,
+                h    : $height
+            }
+
+            if ($caption.length > 0) {
+                item.title = $.trim($titleText + $captionText);
+            }
+
+            items.push(item);
+        });
+
+        // bind click event
+        $folioItems.each(function(i) {
+
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                var options = {
+                    index: i,
+                    showHideOpacity: true
+                }
+
+                // initialize PhotoSwipe
+                var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+                lightBox.init();
+            });
+
+        });
+    };
+
+
+   /* Stat Counter
+    * ------------------------------------------------------ */
+    var clStatCount = function() {
+        
+        var statSection = $(".s-stats"),
+            stats = $(".stats__count");
+
+        statSection.waypoint({
+
+            handler: function(direction) {
+
+                if (direction === "down") {
+
+                    stats.each(function () {
+                        var $this = $(this);
+
+                        $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                            duration: 4000,
+                            easing: 'swing',
+                            step: function (curValue) {
+                                $this.text(Math.ceil(curValue));
+                            }
+                        });
+                    });
+
+                } 
+
+                // trigger once only
+                this.destroy();
+
+            },
+
+            offset: "90%"
+
+        });
+    };
+
+
+   /* Masonry
+    * ---------------------------------------------------- */ 
+    var clMasonryFolio = function () {
+        
+        var containerBricks = $('.masonry');
+
+        containerBricks.imagesLoaded(function () {
+            containerBricks.masonry({
+                itemSelector: '.masonry__brick',
+                resize: true
+            });
+        });
+
+        // layout Masonry after each image loads
+        containerBricks.imagesLoaded().progress( function() {
+            containerBricks.masonry('layout');
+        });
+    };
+
+
+    /* slick slider
+     * ------------------------------------------------------ */
+    var clSlickSlider = function() {
+        
+        $('.testimonials__slider').slick({
+            arrows: false,
+            dots: true,
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            pauseOnFocus: false,
+            autoplaySpeed: 1500,
+            responsive: [
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    };
+
+
+   /* Smooth Scrolling
+    * ------------------------------------------------------ */
+    var clSmoothScroll = function() {
+        
+        $('.smoothscroll').on('click', function (e) {
+            var target = this.hash,
+            $target    = $(target);
+            
+                e.preventDefault();
+                e.stopPropagation();
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+                // check if menu is open
+                if ($('body').hasClass('menu-is-open')) {
+                    $('.header-menu-toggle').trigger('click');
+                }
+
+                window.location.hash = target;
+            });
+        });
+
+    };
+
+
+   /* Placeholder Plugin Settings
+    * ------------------------------------------------------ */
+    var clPlaceholder = function() {
+        $('input, textarea, select').placeholder();  
+    };
+
+
+   /* Alert Boxes
+    * ------------------------------------------------------ */
+    var clAlertBoxes = function() {
+
+        $('.alert-box').on('click', '.alert-box__close', function() {
+            $(this).parent().fadeOut(500);
+        }); 
+
+    };
+
+
+   /* Animate On Scroll
+    * ------------------------------------------------------ */
+    var clAOS = function() {
+        
+        AOS.init( {
+            offset: 200,
+            duration: 600,
+            easing: 'ease-in-sine',
+            delay: 300,
+            once: true,
+            disable: 'mobile'
+        });
+
+    };
+
+
+   /* AjaxChimp
+    * ------------------------------------------------------ */
+    var clAjaxChimp = function() {
+        
+        $('#mc-form').ajaxChimp({
+            language: 'es',
+            url: cfg.mailChimpURL
+        });
+
+        // Mailchimp translation
+        //
+        //  Defaults:
+        //	 'submit': 'Submitting...',
+        //  0: 'We have sent you a confirmation email',
+        //  1: 'Please enter a value',
+        //  2: 'An email address must contain a single @',
+        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+        $.ajaxChimp.translations.es = {
+            'submit': 'Submitting...',
+            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
+            1: '<i class="fas fa-exclamation-circle"></i> You must enter a valid e-mail address.',
+            2: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+            3: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+            4: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.',
+            5: '<i class="fas fa-exclamation-circle"></i> E-mail address is not valid.'
+        } 
+
+    };
+/* Image Gallery start */
+var makeBSS = function (el, options) {
+    var $slideshows = document.querySelectorAll(el), // a collection of all of the slideshow
+        $slideshow = {},
+        Slideshow = {
+            init: function (el, options) {
+                this.counter = 0; // to keep track of current slide
+                this.el = el; // current slideshow container    
+                this.$items = el.querySelectorAll('figure'); // a collection of all of the slides, caching for performance
+                this.numItems = this.$items.length; // total number of slides
+                options = options || {}; // if options object not passed in, then set to empty object 
+                options.auto = options.auto || false; // if options.auto object not passed in, then set to false
+                this.opts = {
+                    auto: (typeof options.auto === "undefined") ? false : options.auto,
+                    speed: (typeof options.auto.speed === "undefined") ? 1500 : options.auto.speed,
+                    pauseOnHover: (typeof options.auto.pauseOnHover === "undefined") ? false : options.auto.pauseOnHover,
+                    fullScreen: (typeof options.fullScreen === "undefined") ? false : options.fullScreen,
+                    swipe: (typeof options.swipe === "undefined") ? false : options.swipe
+                };
+                
+                this.$items[0].classList.add('bss-show'); // add show class to first figure 
+                this.injectControls(el);
+                this.addEventListeners(el);
+                if (this.opts.auto) {
+                    this.autoCycle(this.el, this.opts.speed, this.opts.pauseOnHover);
+                }
+                if (this.opts.fullScreen) {
+                    this.addFullScreen(this.el);
+                }
+                if (this.opts.swipe) {
+                    this.addSwipe(this.el);
+                }
+            },
+            showCurrent: function (i) {
+                // increment or decrement this.counter depending on whether i === 1 or i === -1
+                if (i > 0) {
+                    this.counter = (this.counter + 1 === this.numItems) ? 0 : this.counter + 1;
+                } else {
+                    this.counter = (this.counter - 1 < 0) ? this.numItems - 1 : this.counter - 1;
+                }
+
+                // remove .show from whichever element currently has it 
+                // http://stackoverflow.com/a/16053538/2006057
+                [].forEach.call(this.$items, function (el) {
+                    el.classList.remove('bss-show');
+                });
+  
+                // add .show to the one item that's supposed to have it
+                this.$items[this.counter].classList.add('bss-show');
+            },
+            injectControls: function (el) {
+            // build and inject prev/next controls
+                // first create all the new elements
+                var spanPrev = document.createElement("span"),
+                    spanNext = document.createElement("span"),
+                    docFrag = document.createDocumentFragment();
+        
+                // add classes
+                spanPrev.classList.add('bss-prev');
+                spanNext.classList.add('bss-next');
+        
+                // add contents
+                spanPrev.innerHTML = '&laquo;';
+                spanNext.innerHTML = '&raquo;';
+                
+                // append elements to fragment, then append fragment to DOM
+                docFrag.appendChild(spanPrev);
+                docFrag.appendChild(spanNext);
+                el.appendChild(docFrag);
+            },
+            addEventListeners: function (el) {
+                var that = this;
+                el.querySelector('.bss-next').addEventListener('click', function () {
+                    that.showCurrent(1); // increment & show
+                }, false);
+            
+                el.querySelector('.bss-prev').addEventListener('click', function () {
+                    that.showCurrent(-1); // decrement & show
+                }, false);
+                
+                el.onkeydown = function (e) {
+                    e = e || window.event;
+                    if (e.keyCode === 37) {
+                        that.showCurrent(-1); // decrement & show
+                    } else if (e.keyCode === 39) {
+                        that.showCurrent(1); // increment & show
+                    }
+                };
+            },
+            autoCycle: function (el, speed, pauseOnHover) {
+                var that = this,
+                    interval = window.setInterval(function () {
+                        that.showCurrent(1); // increment & show
+                    }, speed);
+                
+                if (pauseOnHover) {
+                    el.addEventListener('mouseover', function () {
+                        interval = clearInterval(interval);
+                    }, false);
+                    el.addEventListener('mouseout', function () {
+                        interval = window.setInterval(function () {
+                            that.showCurrent(1); // increment & show
+                        }, speed);
+                    }, false);
+                } // end pauseonhover
+                
+            },
+            addFullScreen: function(el){
+                var that = this,
+                fsControl = document.createElement("span");
+                
+                fsControl.classList.add('bss-fullscreen');
+                el.appendChild(fsControl);
+                el.querySelector('.bss-fullscreen').addEventListener('click', function () {
+                    that.toggleFullScreen(el);
+                }, false);
+            },
+            addSwipe: function(el){
+                var that = this,
+                    ht = new Hammer(el);
+                ht.on('swiperight', function(e) {
+                    that.showCurrent(-1); // decrement & show
+                });
+                ht.on('swipeleft', function(e) {
+                    that.showCurrent(1); // increment & show
+                });
+            },
+            toggleFullScreen: function(el){
+                // https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Using_full_screen_mode
+                if (!document.fullscreenElement &&    // alternative standard method
+                    !document.mozFullScreenElement && !document.webkitFullscreenElement &&   
+                    !document.msFullscreenElement ) {  // current working methods
+                    if (document.documentElement.requestFullscreen) {
+                      el.requestFullscreen();
+                    } else if (document.documentElement.msRequestFullscreen) {
+                      el.msRequestFullscreen();
+                    } else if (document.documentElement.mozRequestFullScreen) {
+                      el.mozRequestFullScreen();
+                    } else if (document.documentElement.webkitRequestFullscreen) {
+                      el.webkitRequestFullscreen(el.ALLOW_KEYBOARD_INPUT);
+                    }
+                } else {
+                    if (document.exitFullscreen) {
+                      document.exitFullscreen();
+                    } else if (document.msExitFullscreen) {
+                      document.msExitFullscreen();
+                    } else if (document.mozCancelFullScreen) {
+                      document.mozCancelFullScreen();
+                    } else if (document.webkitExitFullscreen) {
+                      document.webkitExitFullscreen();
+                    }
+                }
+            } // end toggleFullScreen
+            
+        }; // end Slideshow object 
+        
+    // make instances of Slideshow as needed
+    [].forEach.call($slideshows, function (el) {
+        $slideshow = Object.create(Slideshow);
+        $slideshow.init(el, options);
+    });
+};
+var opts = {
+    auto : {
+        speed : 5000, 
+        pauseOnHover : true
+    },
+    fullScreen : true, 
+    swipe : true
+};
+makeBSS('.demo1', opts);
+
+/*Image Gallery End */
+
+   /* Back to Top
+    * ------------------------------------------------------ */
+    var clBackToTop = function() {
+        
+        var pxShow  = 500,         // height on which the button will show
+        fadeInTime  = 400,         // how slow/fast you want the button to show
+        fadeOutTime = 400,         // how slow/fast you want the button to hide
+        scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        goTopButton = $(".cl-go-top")
+        
+        // Show or hide the sticky footer button
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() >= pxShow) {
+                goTopButton.fadeIn(fadeInTime);
+            } else {
+                goTopButton.fadeOut(fadeOutTime);
+            }
+        });
+    };
+
+
+   /* Initialize
+    * ------------------------------------------------------ */
+    (function clInit() {
+        
+        clPreloader();
+        clMenuOnScrolldown();
+        clOffCanvas();
+        clPhotoswipe();
+        clStatCount();
+        clMasonryFolio();
+        clSlickSlider();
+        clSmoothScroll();
+        clPlaceholder();
+        clAlertBoxes();
+        clAOS();
+        clAjaxChimp();
+        clBackToTop();
+
+    })();
+        
+})(jQuery);
